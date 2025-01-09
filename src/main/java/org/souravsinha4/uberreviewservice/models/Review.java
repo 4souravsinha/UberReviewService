dfdef0cc1f5ea7623a6706f4d1a45ee30a16ded1
4souravsinha <75581419+4souravsinha@users.noreply.github.com>
2025-01-09 11:32:47 +0530

    initial db and model setup

---
+++ src/main/java/org/souravsinha4/uberreviewservice/models/Review.java
@@ -0,0 +1,18 @@
+package org.souravsinha4.uberreviewservice.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
